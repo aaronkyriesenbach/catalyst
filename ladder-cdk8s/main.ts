4dc2410fe89833c9f224e1e6b572cba81665b1f7
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-03-09 17:17:18 -0400

    Skip app abstraction for now

--- ladder-cdk8s/main.ts
+++ ladder-cdk8s/main.ts
@@ -1,21 +1,30 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
 
-import BasicApp from '../shared/BasicApp';
+import { Deployment } from "cdk8s-plus-32";
+import IngressRoute from "../shared/IngressRoute";
 
 export class Ladder extends Chart {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        new BasicApp(this, 'app',
-            {
-                containers: [{
-                    image: "ghcr.io/everywall/ladder:latest",
-                    ports: [{ number: 8080 }]
-                }],
-                webPort: { port: 8080 }
-            }
-        )
+        const ladderPort = 8080;
+
+        const deployment = new Deployment(this, 'deployment', {
+            containers: [{
+                image: "ghcr.io/everywall/ladder:latest",
+                ports: [{ number: ladderPort }]
+            }]
+        });
+
+        const service = deployment.exposeViaService({ ports: [{ port: ladderPort }] });
+
+        new IngressRoute(this, 'ingressroute', {
+            serviceName: service.name,
+            port: ladderPort,
+            hostPrefix: 'ladder',
+            useInsecureTransport: true
+        });
     }
 }
 
