35dab439e1f7ae03712493b64a4ff2068982143e
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-03-11 11:37:09 -0400

    Add revisionHistoryLimit for cdk8s deployments

--- shared/AppProps.ts
+++ shared/AppProps.ts
@@ -3,5 +3,6 @@ import { Container } from "./imports/k8s";
 export type AppProps = {
     appName: string
     replicas?: number
+    revisionHistoryLimit?: number
     containers: Container[]
 }
--- shared/Deployment.ts
+++ shared/Deployment.ts
@@ -4,7 +4,7 @@ import { AppProps } from "./AppProps";
 
 export default class Deployment extends KubeDeployment {
     constructor(scope: Construct, id: string, props: AppProps) {
-        const { appName, replicas, containers } = props;
+        const { appName, replicas, revisionHistoryLimit, containers } = props;
 
         super(scope, id, {
             metadata: {
@@ -13,6 +13,7 @@ export default class Deployment extends KubeDeployment {
             spec: {
                 selector: { matchLabels: { app: appName } },
                 replicas: replicas ?? 1,
+                revisionHistoryLimit: revisionHistoryLimit ?? 1,
                 template: {
                     metadata: { labels: { app: appName } },
                     spec: {
