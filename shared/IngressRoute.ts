8f2cfd9f49b11c2f869c3fc3f1a478573b09cccd
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-03-23 11:34:43 -0400

    Add dynamic seedbox script

--- shared/IngressRoute.ts
+++ shared/IngressRoute.ts
@@ -1,69 +1,68 @@
 import {
-    IngressRoute as TraefikIngressRoute,
-    IngressRouteSpecRoutesKind,
-    IngressRouteSpecRoutesMiddlewares,
-    IngressRouteSpecRoutesServicesPort
+  IngressRoute as TraefikIngressRoute,
+  IngressRouteSpecRoutesKind,
+  IngressRouteSpecRoutesMiddlewares,
+  IngressRouteSpecRoutesServicesPort
 } from "./imports/traefik.io.ts";
 import { Construct } from "npm:constructs";
-import { ApiObjectProps } from "npm:cdk8s";
 import { getPorts } from "./Service.ts";
 import { AppProps } from "./AppProps.ts";
 
 export default class IngressRoute extends TraefikIngressRoute {
-    constructor(scope: Construct, id: string, props: IngressRouteProps) {
-        const {
-            appName,
-            namespace,
-            containers = [],
-            initContainers = [],
-            customHostPrefix,
-            useForwardAuth = true,
-            useInsecureTransport,
-            customPort,
-            middlewares
-        } = props;
+  constructor(scope: Construct, id: string, props: IngressRouteProps) {
+    const {
+      appName,
+      containers = [],
+      initContainers = [],
+      customHostPrefix,
+      useForwardAuth = true,
+      useInsecureTransport,
+      customPort,
+      middlewares,
+    } = props;
 
-        const ports = getPorts([...containers, ...initContainers]);
-        const createMiddlewares = useForwardAuth
-            ? [{
-                name: "forwardauth-authelia",
-                namespace: "authelia"
-            }, ...(middlewares ?? [])]
-            : middlewares;
+    const ports = getPorts([...containers, ...initContainers]);
+    const createMiddlewares = useForwardAuth
+      ? [{
+        name: "forwardauth-authelia",
+        namespace: "authelia",
+      }, ...(middlewares ?? [])]
+      : middlewares;
 
-        super(scope, id, {
-            metadata: {
-                name: `${ appName ?? id }${ customHostPrefix ? `-${ customHostPrefix }` : "" }`,
-                namespace: namespace
-            },
-            spec: {
-                entryPoints: ["websecure"],
-                routes: [{
-                    match: `Host(\`${ customHostPrefix ?? appName }.lab53.net\`)`,
-                    kind: IngressRouteSpecRoutesKind.RULE,
-                    middlewares: createMiddlewares,
-                    services: [{
-                        name: appName,
-                        serversTransport: useInsecureTransport
-                            ? "traefik-insecuretransport@kubernetescrd"
-                            : undefined,
-                        port: IngressRouteSpecRoutesServicesPort.fromNumber(
-                            customPort ?? ports[0].port
-                        )
-                    }]
-                }],
-                tls: {
-                    certResolver: "letsencrypt"
-                }
-            }
-        });
-    }
+    super(scope, id, {
+      metadata: {
+        name: `${appName ?? id}${
+          customHostPrefix ? `-${customHostPrefix}` : ""
+        }`,
+      },
+      spec: {
+        entryPoints: ["websecure"],
+        routes: [{
+          match: `Host(\`${customHostPrefix ?? appName}.lab53.net\`)`,
+          kind: IngressRouteSpecRoutesKind.RULE,
+          middlewares: createMiddlewares,
+          services: [{
+            name: appName,
+            serversTransport: useInsecureTransport
+              ? "traefik-insecuretransport@kubernetescrd"
+              : undefined,
+            port: IngressRouteSpecRoutesServicesPort.fromNumber(
+              customPort ?? ports[0].port,
+            ),
+          }],
+        }],
+        tls: {
+          certResolver: "letsencrypt",
+        },
+      },
+    });
+  }
 }
 
-export type IngressRouteProps = AppProps & ApiObjectProps & {
-    customPort?: number;
-    customHostPrefix?: string;
-    useForwardAuth?: boolean;
-    useInsecureTransport?: boolean;
-    middlewares?: IngressRouteSpecRoutesMiddlewares[];
+export type IngressRouteProps = AppProps & {
+  customPort?: number;
+  customHostPrefix?: string;
+  useForwardAuth?: boolean;
+  useInsecureTransport?: boolean;
+  middlewares?: IngressRouteSpecRoutesMiddlewares[];
 };
