a84e9f1810ba0bfb83438d98d2b8da51e5db5bd4
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-03-23 11:34:43 -0400

    Add dynamic seedbox script

--- transmission/main.ts
+++ transmission/main.ts
@@ -8,162 +8,195 @@ import Deployment from "../shared/Deployment.ts";
 import Service from "../shared/Service.ts";
 import IngressRoute from "../shared/IngressRoute.ts";
 import GeneratedPassword from "../shared/GeneratedPassword.ts";
+import ConfigMap from "../shared/ConfigMap.ts";
 
 export class Transmission extends Chart {
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        new KubeNamespace(this, "namespace", {
-            metadata: {
-                name: "transmission",
-                labels: {
-                    "pod-security.kubernetes.io/enforce": "privileged"
-                }
-            }
-        });
+    new KubeNamespace(this, "namespace", {
+      metadata: {
+        name: "transmission",
+        labels: {
+          "pod-security.kubernetes.io/enforce": "privileged",
+        },
+      },
+    });
 
-        const configPVC = new ConfigPVC(this, "configpvc", { name: "config-pvc" });
+    const configPVC = new ConfigPVC(this, "configpvc", { name: "config-pvc" });
 
-        const gluetunRoleConfig = new GeneratedPassword(this, "gluetun-role-config", {
-            name: "gluetun-role-config",
-            secretTemplate: {
-                type: "Opaque",
-                stringData: {
-                    "config.toml": Deno.readTextFileSync("gluetun-role-config.toml"),
-                    key: "$(value)"
-                }
-            }
-        });
+    const gluetunRoleConfig = new GeneratedPassword(
+      this,
+      "gluetun-role-config",
+      {
+        name: "gluetun-role-config",
+        secretTemplate: {
+          type: "Opaque",
+          stringData: {
+            "config.toml": Deno.readTextFileSync("gluetun-role-config.toml"),
+            key: "$(value)",
+          },
+        },
+      },
+    );
 
-        const transmissionApp: AppProps = {
-            appName: "transmission",
-            createNASVolume: true,
-            volumes: [{
-                name: configPVC.name,
-                persistentVolumeClaim: {
-                    claimName: configPVC.name
-                }
-            }, {
-                name: gluetunRoleConfig.name,
-                secret: {
-                    secretName: gluetunRoleConfig.name
-                }
-            }],
-            containers: [{
-                name: "transmission",
-                image: "lscr.io/linuxserver/transmission:4.0.6-r2-ls281",
-                env: DEFAULT_LSCR_ENV,
-                volumeMounts: [
-                    {
-                        name: DEFAULT_NAS_VOLUME_NAME,
-                        mountPath: "/downloads",
-                        subPath: "downloads"
-                    },
-                    {
-                        name: DEFAULT_NAS_VOLUME_NAME,
-                        mountPath: "/watch",
-                        subPath: "downloads/watch"
-                    },
-                    {
-                        name: configPVC.name,
-                        mountPath: "/config"
-                    }
-                ]
-            }],
-            initContainers: [{
-                name: "gluetun",
-                image: "qmcgaw/gluetun:v3.40.0",
-                restartPolicy: "Always",
-                securityContext: {
-                    capabilities: {
-                        add: ["NET_ADMIN"]
-                    }
-                },
-                ports: [{ name: "web", containerPort: 9091 }],
-                env: [
-                    {
-                        name: "VPN_SERVICE_PROVIDER",
-                        value: "protonvpn"
-                    },
-                    {
-                        name: "VPN_TYPE",
-                        value: "wireguard"
-                    },
-                    {
-                        name: "SERVER_COUNTRIES",
-                        value: "United States"
-                    },
-                    {
-                        name: "PORT_FORWARD_ONLY",
-                        value: "on"
-                    },
-                    {
-                        name: "VPN_PORT_FORWARDING",
-                        value: "on"
-                    },
-                    {
-                        name: "WIREGUARD_PRIVATE_KEY",
-                        valueFrom: {
-                            secretKeyRef: {
-                                name: "wireguard-private-key",
-                                key: "key"
-                            }
-                        }
-                    }
-                ],
-                volumeMounts: [
-                    {
-                        name: gluetunRoleConfig.name,
-                        mountPath: "/gluetun/auth"
-                    },
-                    {
-                        name: configPVC.name,
-                        mountPath: "/gluetun"
-                    }
-                ]
+    const seedboxCm = new ConfigMap(this, "seedbox-cm", {
+      name: "script",
+      data: {
+        "dynamic-seedbox.sh": Deno.readTextFileSync("dynamic-seedbox.sh"),
+      },
+    });
+
+    const transmissionApp: AppProps = {
+      appName: "transmission",
+      createNASVolume: true,
+      volumes: [{
+        name: configPVC.name,
+        persistentVolumeClaim: {
+          claimName: configPVC.name,
+        },
+      }, {
+        name: gluetunRoleConfig.name,
+        secret: {
+          secretName: gluetunRoleConfig.name,
+        },
+      }, {
+        name: seedboxCm.name,
+        configMap: {
+          name: seedboxCm.name,
+          defaultMode: 0o755,
+        },
+      }],
+      containers: [{
+        name: "transmission",
+        image: "lscr.io/linuxserver/transmission:4.0.6-r2-ls281",
+        env: DEFAULT_LSCR_ENV,
+        volumeMounts: [
+          {
+            name: DEFAULT_NAS_VOLUME_NAME,
+            mountPath: "/downloads",
+            subPath: "downloads",
+          },
+          {
+            name: DEFAULT_NAS_VOLUME_NAME,
+            mountPath: "/watch",
+            subPath: "downloads/watch",
+          },
+          {
+            name: configPVC.name,
+            mountPath: "/config",
+          },
+        ],
+      }],
+      initContainers: [{
+        name: "gluetun",
+        image: "qmcgaw/gluetun:v3.40.0",
+        restartPolicy: "Always",
+        securityContext: {
+          capabilities: {
+            add: ["NET_ADMIN"],
+          },
+        },
+        ports: [{ name: "web", containerPort: 9091 }],
+        env: [
+          {
+            name: "VPN_SERVICE_PROVIDER",
+            value: "protonvpn",
+          },
+          {
+            name: "VPN_TYPE",
+            value: "wireguard",
+          },
+          {
+            name: "SERVER_COUNTRIES",
+            value: "United States",
+          },
+          {
+            name: "PORT_FORWARD_ONLY",
+            value: "on",
+          },
+          {
+            name: "VPN_PORT_FORWARDING",
+            value: "on",
+          },
+          {
+            name: "WIREGUARD_PRIVATE_KEY",
+            valueFrom: {
+              secretKeyRef: {
+                name: "wireguard-private-key",
+                key: "key",
+              },
             },
-                {
-                    name: "gluetrans",
-                    image: "miklosbagi/gluetrans:v0.3.5",
-                    restartPolicy: "Always",
-                    env: [
-                        {
-                            name: "GLUETUN_CONTROL_ENDPOINT",
-                            value: "http://localhost:8000"
-                        },
-                        {
-                            name: "GLUETUN_CONTROL_API_KEY",
-                            valueFrom: {
-                                secretKeyRef: {
-                                    name: gluetunRoleConfig.name,
-                                    key: "key"
-                                }
-                            }
-                        },
-                        {
-                            name: "GLUETUN_HEALTH_ENDPOINT",
-                            value: "http://localhost:9999"
-                        },
-                        {
-                            name: "TRANSMISSION_ENDPOINT",
-                            value: "http://localhost:9091/transmission/rpc"
-                        },
-                        {
-                            name: "TRANSMISSION_USER",
-                            value: "transmission"
-                        },
-                        {
-                            name: "TRANSMISSION_PASS",
-                            value: "transmission"
-                        }
-                    ]
-                }]
-        };
+          },
+        ],
+        volumeMounts: [
+          {
+            name: gluetunRoleConfig.name,
+            mountPath: "/gluetun/auth",
+          },
+          {
+            name: configPVC.name,
+            mountPath: "/gluetun",
+          },
+        ],
+      }, {
+        name: "gluetrans",
+        image: "miklosbagi/gluetrans:v0.3.5",
+        restartPolicy: "Always",
+        env: [
+          {
+            name: "GLUETUN_CONTROL_ENDPOINT",
+            value: "http://localhost:8000",
+          },
+          {
+            name: "GLUETUN_CONTROL_API_KEY",
+            valueFrom: {
+              secretKeyRef: {
+                name: gluetunRoleConfig.name,
+                key: "key",
+              },
+            },
+          },
+          {
+            name: "GLUETUN_HEALTH_ENDPOINT",
+            value: "http://localhost:9999",
+          },
+          {
+            name: "TRANSMISSION_ENDPOINT",
+            value: "http://localhost:9091/transmission/rpc",
+          },
+          {
+            name: "TRANSMISSION_USER",
+            value: "transmission",
+          },
+          {
+            name: "TRANSMISSION_PASS",
+            value: "transmission",
+          },
+        ],
+      }, {
+        name: "seedbox-updater",
+        image: "alpine/curl:8.12.1",
+        restartPolicy: "Always",
+        command: ["/mnt/dynamic-seedbox.sh"],
+        volumeMounts: [{
+          name: seedboxCm.name,
+          mountPath: "/mnt/dynamic-seedbox.sh",
+          subPath: "dynamic-seedbox.sh",
+        }, {
+          name: configPVC.name,
+          mountPath: "/config",
+        }],
+      }],
+    };
 
-        new Deployment(this, "deployment", transmissionApp);
-        new Service(this, "service", transmissionApp);
-        new IngressRoute(this, "ingress", { useInsecureTransport: true, ...transmissionApp });
-    }
+    new Deployment(this, "deployment", transmissionApp);
+    new Service(this, "service", transmissionApp);
+    new IngressRoute(this, "ingress", {
+      useInsecureTransport: true,
+      ...transmissionApp,
+    });
+  }
 }
 
 const app = new App();
