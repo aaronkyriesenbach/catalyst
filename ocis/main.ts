5a1ea97699ebe2a585988ee178ecce24a4e56e1c
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-03-24 14:08:54 -0400

    Add generated admin pass

--- ocis/main.ts
+++ ocis/main.ts
@@ -2,18 +2,30 @@ import { Construct } from "npm:constructs";
 import { App, Chart } from "npm:cdk8s";
 import { Ocis as OCISChart } from "./imports/ocis.ts";
 import IngressRoute from "../shared/IngressRoute.ts";
+import GeneratedPassword from "../shared/GeneratedPassword.ts";
 
 export class OCIS extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    const adminPass = new GeneratedPassword(this, "adminpass", {
+      name: "admin-pass",
+      secretTemplate: {
+        type: "Opaque",
+        stringData: {
+          password: "$(value)",
+          "user-id": crypto.randomUUID(),
+        },
+      },
+    });
 
     new OCISChart(this, "ocis", {
       namespace: "ocis",
       releaseName: "ocis",
       values: {
         externalDomain: "ocis.lab53.net",
-        insecure: {
-          ocisHttpApiInsecure: true,
+        secretRefs: {
+          adminUserSecretRef: adminPass.name,
         },
       },
     });
