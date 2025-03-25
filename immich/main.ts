b116dd20306cf95b7cd59ffb1eb831be5237432e
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-03-25 10:01:15 -0400

    Add plex

--- immich/main.ts
+++ immich/main.ts
@@ -7,78 +7,80 @@ import GeneratedPassword from "../shared/GeneratedPassword.ts";
 import NASVolume from "../shared/NASVolume.ts";
 
 export class Immich extends Chart {
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id, { namespace: "immich" });
 
-        const postgresSecret = new GeneratedPassword(this, "postgres-creds", {
-            name: "immich-user",
-            secretTemplate: {
-                stringData: {
-                    username: "immich",
-                    password: "$(value)"
-                }
-            }
-        });
+    const postgresSecret = new GeneratedPassword(this, "postgres-creds", {
+      name: "immich-user",
+      secretTemplate: {
+        stringData: {
+          username: "immich",
+          password: "$(value)",
+        },
+      },
+    });
 
-        new CNPGCluster(this, "cluster", {
-            appName: "immich",
-            imageName: "ghcr.io/tensorchord/cloudnative-pgvecto.rs:16-v0.3.0", // Immich requires pgvecto.rs < v0.4.0
-            postgresql: {
-                sharedPreloadLibraries: ["vectors.so"]
-            },
-            passwordSecret: {
+    new CNPGCluster(this, "cluster", {
+      appName: "immich",
+      imageName: "ghcr.io/tensorchord/cloudnative-pgvecto.rs:16-v0.3.0", // Immich requires pgvecto.rs < v0.4.0
+      postgresql: {
+        sharedPreloadLibraries: ["vectors.so"],
+      },
+      passwordSecret: {
+        name: postgresSecret.name,
+        key: "password",
+      },
+      superuser: true, // Immich requires superuser access for database administration and automatic backups
+    });
+
+    const nasVolume = new NASVolume(this, "nasvolume", {
+      size: "1Ti",
+      customNASPath: "/mnt/tank/data/pictures",
+    });
+
+    const chart = new ImmichChart(this, "chart", {
+      namespace: "immich",
+      releaseName: "immich",
+      values: {
+        env: {
+          DB_HOSTNAME: "immich-cluster-rw",
+          DB_PASSWORD: {
+            valueFrom: {
+              secretKeyRef: {
                 name: postgresSecret.name,
-                key: "password"
+                key: "password",
+              },
             },
-            superuser: true // Immich requires superuser access for database administration and automatic backups
-        });
+          },
+        },
+        immich: {
+          persistence: {
+            library: {
+              existingClaim: nasVolume.getPVCName(),
+            },
+          },
+        },
+        image: {
+          tag: "v1.129.0",
+        },
+        redis: {
+          enabled: true,
+        },
+      },
+    });
 
-        const nasVolume = new NASVolume(this, "nasvolume", {
-            size: "1Ti",
-            customNASPath: "/mnt/tank/data/pictures"
-        });
+    const serviceApiObject = chart.apiObjects.find((c) =>
+      c.kind === "Service" && c.name.includes("-server")
+    );
 
-        const chart = new ImmichChart(this, "chart", {
-            namespace: "immich",
-            releaseName: "immich",
-            values: {
-                env: {
-                    DB_HOSTNAME: "immich-cluster-rw",
-                    DB_PASSWORD: {
-                        valueFrom: {
-                            secretKeyRef: {
-                                name: postgresSecret.name,
-                                key: "password"
-                            }
-                        }
-                    }
-                },
-                immich: {
-                    persistence: {
-                        library: {
-                            existingClaim: nasVolume.pvcName
-                        }
-                    }
-                },
-                image: {
-                    tag: "v1.129.0"
-                },
-                redis: {
-                    enabled: true
-                }
-            }
-        });
-
-        const serviceApiObject = chart.apiObjects.find(c => c.kind === "Service" && c.name.includes("-server"))
-
-        new IngressRoute(this, "ingress", {
-            appName: serviceApiObject.name,
-            customHostPrefix: "immich",
-            useInsecureTransport: true,
-            useForwardAuth: false,
-            customPort: 2283
-        });
-    }
+    new IngressRoute(this, "ingress", {
+      appName: serviceApiObject.name,
+      customHostPrefix: "immich",
+      useInsecureTransport: true,
+      useForwardAuth: false,
+      customPort: 2283,
+    });
+  }
 }
 
 const app = new App();
