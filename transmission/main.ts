c16743609afd1e435f6ace8ca29d54d7b7339fd9
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-04-04 12:27:18 -0400

    Many changes

--- transmission/main.ts
+++ transmission/main.ts
@@ -3,6 +3,7 @@ import { Chart } from "npm:cdk8s";
 import { KubeNamespace } from "../shared/imports/k8s.ts";
 import { Lab53App } from "../shared/helpers.ts";
 import TransmissionApp from "./TransmissionApp.ts";
+import GeneratedPassword from "../shared/GeneratedPassword.ts";
 
 export class Transmission extends Chart {
   constructor(scope: Construct, id: string) {
@@ -17,16 +18,25 @@ export class Transmission extends Chart {
       },
     });
 
+    const writerPassword = new GeneratedPassword(this, {
+      name: "writer-password",
+    });
+    const readerPassword = new GeneratedPassword(this, {
+      name: "reader-password",
+    });
+
     new TransmissionApp(this, {
       name: "writer",
       protonSecretName: "proton-openvpn-creds",
       downloadSubpath: "downloads",
+      transmissionPasswordSecret: writerPassword.name,
     });
 
     new TransmissionApp(this, {
       name: "reader",
       protonSecretName: "proton-openvpn-creds",
       downloadSubpath: "downloads/reader",
+      transmissionPasswordSecret: readerPassword.name,
       postStartHook: {
         exec: {
           command: ["/bin/sh", "/etc/openvpn/custom/update-seedbox.sh"],
