a5e4898517842865958b6d039c95ae4f3202189d
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-04-14 13:31:45 -0400

    Add excalidraw

---
+++ excalidraw/main.ts
@@ -0,0 +1,29 @@
+import { Chart } from "npm:cdk8s";
+import { Construct } from "npm:constructs";
+import Application from "../shared/Application.ts";
+import { Lab53App } from "../shared/helpers.ts";
+
+export class Excalidraw extends Chart {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new Application(this, {
+      name: "excalidraw",
+      podSpecProps: {
+        containers: [{
+          name: "excalidraw",
+          image: "excalidraw/excalidraw:latest",
+          ports: [{ containerPort: 80, name: "web" }],
+        }],
+      },
+      webPort: 80,
+      ingressRouteSpec: {
+        customHostPrefix: "draw",
+      },
+    });
+  }
+}
+
+const app = new Lab53App();
+new Excalidraw(app, "excalidraw");
+app.synth();
