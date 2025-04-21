a1049df3a239f637464f3d2e8c035cba08f03b45
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-04-20 18:47:47 -0700

    Move shared apps to base layer, install ESO

---
+++ external/external-secrets/main.ts
@@ -0,0 +1,6 @@
+import { Lab53App } from "../../shared/helpers.ts";
+import { ExternalSecrets } from "../../base/external-secrets/main.ts";
+
+const app = new Lab53App();
+new ExternalSecrets(app, "external-secrets-external");
+app.synth();
