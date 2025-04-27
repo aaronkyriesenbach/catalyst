4bff91b505965f7a5ea3c41244045700a6fdf903
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-04-27 19:54:33 -0400

    Allow service updates

--- scripts/generate_kube_vip.sh
+++ scripts/generate_kube_vip.sh
@@ -10,4 +10,4 @@ if [ -f kube-vip.yaml ]; then
 fi
 
 docker run --rm --network host ghcr.io/kube-vip/kube-vip:$KVVERSION manifest daemonset --interface $INTERFACE --address $VIP --inCluster --taint --controlplane --services --arp --leaderElection > kube-vip.yaml
-docker run --rm --network host ghcr.io/kube-vip/kube-vip:$KVVERSION manifest rbac >> kube-vip.yaml
+docker run --rm --network host ghcr.io/kube-vip/kube-vip:$KVVERSION manifest rbac --interface $INTERFACE --address $VIP --inCluster --controlplane --services --arp --leaderElection >> kube-vip.yaml
