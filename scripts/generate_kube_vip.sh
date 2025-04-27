aac501fda145749010807524f80707153a5eda61
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-04-27 17:15:15 -0400

    Add kube-vip

---
+++ scripts/generate_kube_vip.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+export VIP=192.168.53.100
+export INTERFACE=eth0
+
+export KVVERSION=v0.9.0
+
+if [ -f kube-vip.yaml ]; then
+  rm kube-vip.yaml
+fi
+
+docker run --rm --network host ghcr.io/kube-vip/kube-vip:$KVVERSION manifest daemonset --interface $INTERFACE --address $VIP --inCluster --taint --controlplane --services --arp --leaderElection > kube-vip.yaml
+docker run --rm --network host ghcr.io/kube-vip/kube-vip:$KVVERSION manifest rbac >> kube-vip.yaml
