8efa6bfc2ce1adcbc9e3301b4b84fbb23a6a2b74
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-04-29 12:18:22 -0400

    Allow selection between staging/non-staging

--- internal/cert-manager/main.ts
+++ internal/cert-manager/main.ts
@@ -2,5 +2,5 @@ import { Lab53App } from "../../shared/helpers.ts";
 import { CertManager } from "../../base/cert-manager/main.ts";
 
 const app = new Lab53App();
-new CertManager(app, "cert-manager-internal");
+new CertManager(app, "cert-manager-internal", true);
 app.synth();
