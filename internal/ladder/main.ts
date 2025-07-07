fbd895341cebb40701d7436a0b7cf2ae97116e2b
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-07-06 20:44:13 -0400

    Remove test PVC

--- internal/ladder/main.ts
+++ internal/ladder/main.ts
@@ -2,23 +2,14 @@ import { Construct } from "npm:constructs";
 import { Chart } from "npm:cdk8s";
 import { Lab53App } from "../../shared/helpers.ts";
 import Application from "../../shared/Application.ts";
-import ConfigPVC from "../../shared/ConfigPVC.ts";
 
 export class Ladder extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const testPVC = new ConfigPVC(this, { name: "test" });
-
     new Application(this, {
       name: "ladder",
       podSpecProps: {
-        volumes: [{
-          name: testPVC.name,
-          persistentVolumeClaim: {
-            claimName: testPVC.name,
-          },
-        }],
         containers: [{
           name: "ladder",
           image: "ghcr.io/everywall/ladder:latest",
@@ -27,10 +18,6 @@ export class Ladder extends Chart {
             name: "RULESET",
             value:
               "https://raw.githubusercontent.com/everywall/ladder-rules/main/ruleset.yaml",
-          }],
-          volumeMounts: [{
-            name: testPVC.name,
-            mountPath: "/test",
           }],
         }],
       },
