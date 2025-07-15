60cfa859ffbd0e7534b435a70bb3f2710d2ee6f0
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-07-15 10:17:40 -0400

    Use internal pull-through registries

--- external/excalidraw/main.ts
+++ external/excalidraw/main.ts
@@ -12,7 +12,7 @@ export class Excalidraw extends Chart {
       podSpecProps: {
         containers: [{
           name: "excalidraw",
-          image: "registry.int.lab53.net/excalidraw/excalidraw:latest",
+          image: "hub.int.lab53.net/excalidraw/excalidraw:latest",
           ports: [{ containerPort: 80, name: "web" }],
         }],
       },
