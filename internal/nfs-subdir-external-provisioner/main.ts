54cea891cd5467b18958d57e4081f8519e4d8daf
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-08-21 15:00:37 -0400

    Add NFS external provisioner

---
+++ internal/nfs-subdir-external-provisioner/main.ts
@@ -0,0 +1,31 @@
+import { Chart } from "npm:cdk8s";
+import { Lab53App } from "../../shared/helpers.ts";
+import { Construct } from "npm:constructs";
+import { HelmChart } from "../../shared/HelmChart.ts";
+import { stringify } from "npm:yaml@2.7.1";
+
+class NFSSubdirExternalProvisioner extends Chart {
+  constructor(scope: Construct) {
+    super(scope, crypto.randomUUID());
+
+    new HelmChart(this, {
+      name: "nfs-subdir-external-provisioner",
+      repo:
+        "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/",
+      version: "4.0.18",
+      values: stringify({
+        nfs: {
+          server: "192.168.53.40",
+          path: "/mnt/tank/data/cluster",
+        },
+        storageClass: {
+          defaultClass: true,
+        },
+      }),
+    });
+  }
+}
+
+const app = new Lab53App();
+new NFSSubdirExternalProvisioner(app);
+app.synth();
