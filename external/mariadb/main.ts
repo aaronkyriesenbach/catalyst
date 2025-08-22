ed029a3069c6890d7db706a57485b7ecf0040e5a
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2025-08-22 16:54:14 -0400

    Add bookstack

---
+++ external/mariadb/main.ts
@@ -0,0 +1,26 @@
+import { Chart } from "npm:cdk8s";
+import { Construct } from "npm:constructs";
+import { Lab53App } from "../../shared/helpers.ts";
+import { HelmChart } from "../../shared/HelmChart.ts";
+
+class MariaDB extends Chart {
+  constructor(scope: Construct) {
+    super(scope, crypto.randomUUID());
+
+    new HelmChart(this, {
+      name: "mariadb-operator-crds",
+      repo: "https://helm.mariadb.com/mariadb-operator",
+      version: "25.8.3",
+    });
+
+    new HelmChart(this, {
+      name: "mariadb-operator",
+      repo: "https://helm.mariadb.com/mariadb-operator",
+      version: "25.8.3",
+    });
+  }
+}
+
+const app = new Lab53App();
+new MariaDB(app);
+app.synth();
