00432c8b9c24d323253faacb624a8c7184caef47
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2025-08-26 14:09:41 -0400

    Add roles

--- external/redis/main.ts
+++ external/redis/main.ts
@@ -3,7 +3,14 @@ import { Construct } from "npm:constructs";
 import { Lab53App } from "../../shared/helpers.ts";
 import { HelmChart } from "../../shared/HelmChart.ts";
 import { RedisEnterpriseCluster } from "../../shared/imports/rec-app.redislabs.com.ts";
-import { KubeRole, KubeRoleBinding } from "../../shared/imports/k8s.ts";
+import {
+  KubeClusterRole,
+  KubeClusterRoleBinding,
+  KubeNamespace,
+  KubeRole,
+  KubeRoleBinding,
+} from "../../shared/imports/k8s.ts";
+import { stringify } from "npm:yaml@2.7.1";
 
 class Redis extends Chart {
   constructor(scope: Construct) {
@@ -14,6 +21,13 @@ class Redis extends Chart {
       namespace: "redis",
       repo: "https://helm.redis.io",
       version: "7.22.0-17",
+      values: stringify({
+        operator: {
+          image: {
+            repository: "hub.int.lab53.net/redislabs/operator",
+          },
+        },
+      }),
     });
 
     const recNamespace = "redis";
@@ -28,10 +42,31 @@ class Redis extends Chart {
         persistentSpec: {
           enabled: true,
         },
+        redisEnterpriseImageSpec: {
+          repository: "hub.int.lab53.net/redislabs/redis",
+        },
+        bootstrapperImageSpec: {
+          repository: "hub.int.lab53.net/redislabs/operator",
+        },
+        redisEnterpriseServicesRiggerImageSpec: {
+          repository: "hub.int.lab53.net/redislabs/k8s-controller",
+        },
       },
     });
 
     const managedNamespaces = ["authelia", "outline"];
+
+    managedNamespaces.map(
+      (namespace) =>
+        new KubeNamespace(this, crypto.randomUUID(), {
+          metadata: {
+            name: namespace,
+            labels: {
+              "redis.enabled": "true",
+            },
+          },
+        }),
+    );
 
     // Roles and bindings created per https://redis.io/docs/latest/operate/kubernetes/7.4.6/re-clusters/multi-namespace/
     managedNamespaces.map(
@@ -147,6 +182,43 @@ class Redis extends Chart {
           },
         }),
     );
+
+    new KubeClusterRole(this, crypto.randomUUID(), {
+      metadata: {
+        name: "redis-enterprise-operator-consumer-ns",
+        labels: {
+          app: "redis-enterprise",
+        },
+      },
+      rules: [
+        {
+          apiGroups: [""],
+          resources: ["namespaces"],
+          verbs: ["list", "watch"],
+        },
+      ],
+    });
+
+    new KubeClusterRoleBinding(this, crypto.randomUUID(), {
+      metadata: {
+        name: "redis-enterprise-operator-consumer-ns",
+        labels: {
+          app: "redis-enterprise",
+        },
+      },
+      subjects: [
+        {
+          kind: "ServiceAccount",
+          name: "redis-enterprise-operator",
+          namespace: recNamespace,
+        },
+      ],
+      roleRef: {
+        kind: "ClusterRole",
+        name: "redis-enterprise-operator-consumer-ns",
+        apiGroup: "rbac.authorization.k8s.io",
+      },
+    });
   }
 }
 
