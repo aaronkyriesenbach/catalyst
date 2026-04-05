3dac36d3d1a55fd98505435aa6ac935d1b56ae3c
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2026-04-05 19:32:48 -0400

    feat: add issuers

--- apps/cert-manager.ts
+++ apps/cert-manager.ts
@@ -1,4 +1,4 @@
-import { ConfigMap } from "kubernetes-models/v1";
+import { ClusterIssuer } from "@kubernetes-models/cert-manager/cert-manager.io/v1";
 import { AppConfig } from "../types";
 
 const chart = {
@@ -17,9 +17,71 @@ const chart = {
   },
 };
 
+const stagingIssuer = new ClusterIssuer({
+  metadata: {
+    name: "letsencrypt-staging",
+  },
+  spec: {
+    acme: {
+      server: "https://acme-staging-v02.api.letsencrypt.org/directory",
+      privateKeySecretRef: {
+        name: "letsencrypt-staging-key",
+      },
+      solvers: [
+        {
+          dns01: {
+            route53: {
+              region: "us-east-1",
+              accessKeyIDSecretRef: {
+                name: "route53-creds",
+                key: "access-key-id",
+              },
+              secretAccessKeySecretRef: {
+                name: "route53-creds",
+                key: "secret-access-key",
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+});
+
+const prodIssuer = new ClusterIssuer({
+  metadata: {
+    name: "letsencrypt-prod",
+  },
+  spec: {
+    acme: {
+      server: "https://acme-v02.api.letsencrypt.org/directory",
+      privateKeySecretRef: {
+        name: "letsencrypt-prod-key",
+      },
+      solvers: [
+        {
+          dns01: {
+            route53: {
+              region: "us-east-1",
+              accessKeyIDSecretRef: {
+                name: "route53-creds",
+                key: "access-key-id",
+              },
+              secretAccessKeySecretRef: {
+                name: "route53-creds",
+                key: "secret-access-key",
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+});
+
 const config: AppConfig = {
   name: "cert-manager",
-  extraResources: [chart],
+  extraResources: [chart, stagingIssuer, prodIssuer],
 };
 
 export default config;
