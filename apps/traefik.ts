e81ebdba53d276639521e87fc0a73665fe983008
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2026-04-06 12:50:14 -0400

    fix: add insecure transport

--- apps/traefik.ts
+++ apps/traefik.ts
@@ -3,10 +3,17 @@ import { certs } from "./traefik/certs";
 import { httpRedirect } from "./traefik/redirect";
 import { gateway } from "./traefik/gateway";
 import { routes } from "./traefik/routes";
+import { insecureTransport } from "./traefik/transports";
 
 const config: AppConfig = {
   name: "traefik",
-  extraResources: [...certs, gateway, httpRedirect, ...routes],
+  extraResources: [
+    ...certs,
+    gateway,
+    httpRedirect,
+    insecureTransport,
+    ...routes,
+  ],
 };
 
 export default config;
