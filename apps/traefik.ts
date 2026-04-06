dda3701ca2fca9a16e5c426741785a9e04900803
Aaron Ky-Riesenbach <aaron@kyriesenba.ch>
2026-04-05 21:23:23 -0400

    feat: add staging certs + http redirect routes

--- apps/traefik.ts
+++ apps/traefik.ts
@@ -1,8 +1,10 @@
 import {
   GatewayClass,
   Gateway,
+  HTTPRoute,
 } from "@kubernetes-models/gateway-api/gateway.networking.k8s.io/v1";
 import { AppConfig } from "../types";
+import { Certificate } from "@kubernetes-models/cert-manager/cert-manager.io/v1";
 
 const gatewayClass = new GatewayClass({
   metadata: {
@@ -10,6 +12,34 @@ const gatewayClass = new GatewayClass({
   },
   spec: {
     controllerName: "traefik.io/gateway-controller",
+  },
+});
+
+const internalCertStaging = new Certificate({
+  metadata: {
+    name: "int-lab53-net-staging",
+  },
+  spec: {
+    secretName: "int-lab53-net-staging",
+    dnsNames: ["int.lab53.net", "*.int.lab53.net"],
+    issuerRef: {
+      name: "letsencrypt-staging",
+      kind: "ClusterIssuer",
+    },
+  },
+});
+
+const externalCertStaging = new Certificate({
+  metadata: {
+    name: "lab53-net-staging",
+  },
+  spec: {
+    secretName: "lab53-net-staging",
+    dnsNames: ["lab53.net", "*.lab53.net"],
+    issuerRef: {
+      name: "letsencrypt-staging",
+      kind: "ClusterIssuer",
+    },
   },
 });
 
@@ -21,14 +51,100 @@ const gateway = new Gateway({
     gatewayClassName: "traefik",
     listeners: [
       {
+        name: "http-int",
+        protocol: "HTTP",
+        port: 80,
+        hostname: "*.int.lab53.net",
+      },
+      {
         name: "http",
         protocol: "HTTP",
         port: 80,
+        hostname: "*.lab53.net",
+      },
+      {
+        name: "https-int",
+        protocol: "HTTPS",
+        port: 443,
+        hostname: "*.int.lab53.net",
+        tls: {
+          certificateRefs: [
+            {
+              kind: "Secret",
+              name: "int-lab53-net-staging",
+            },
+          ],
+        },
       },
       {
         name: "https",
         protocol: "HTTPS",
         port: 443,
+        hostname: "*.lab53.net",
+        tls: {
+          certificateRefs: [
+            {
+              kind: "Secret",
+              name: "lab53-net-staging",
+            },
+          ],
+        },
+      },
+    ],
+  },
+});
+
+const internalRedirect = new HTTPRoute({
+  metadata: {
+    name: "http-redirect-int",
+  },
+  spec: {
+    parentRefs: [
+      {
+        name: "traefik",
+        sectionName: "http-int",
+      },
+    ],
+    hostnames: ["*.int.lab53.net"],
+    rules: [
+      {
+        filters: [
+          {
+            type: "RequestRedirect",
+            requestRedirect: {
+              scheme: "https",
+              statusCode: 301,
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
+
+const externalRedirect = new HTTPRoute({
+  metadata: {
+    name: "http-redirect",
+  },
+  spec: {
+    parentRefs: [
+      {
+        name: "traefik",
+        sectionName: "http",
+      },
+    ],
+    hostnames: ["*.lab53.net"],
+    rules: [
+      {
+        filters: [
+          {
+            type: "RequestRedirect",
+            requestRedirect: {
+              scheme: "https",
+              statusCode: 301,
+            },
+          },
+        ],
       },
     ],
   },
@@ -36,7 +152,14 @@ const gateway = new Gateway({
 
 const config: AppConfig = {
   name: "traefik",
-  extraResources: [gatewayClass, gateway],
+  extraResources: [
+    gatewayClass,
+    internalCertStaging,
+    externalCertStaging,
+    gateway,
+    internalRedirect,
+    externalRedirect,
+  ],
 };
 
 export default config;
