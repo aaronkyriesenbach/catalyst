7391f566267f77aeb094adbbb6e450f79e8e2f4b
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2026-04-16 16:28:03 -0400

    feat: add registry

---
+++ apps/registry.ts
@@ -0,0 +1,46 @@
+import type { WorkloadApp } from "../types";
+import {
+  applyModifiers,
+  withNasMounts,
+  withSecurityDefaults,
+} from "../modifiers";
+
+const base: WorkloadApp = {
+  kind: "workload",
+  name: "registry",
+  podSpec: {
+    containers: [
+      {
+        name: "main",
+        image: "registry:2",
+        env: [
+          {
+            name: "REGISTRY_PROXY_REMOTEURL",
+            value: "https://registry-1.docker.io",
+          },
+        ],
+        ports: [{ name: "http", containerPort: 5000 }],
+        livenessProbe: {
+          httpGet: { path: "/", port: 5000 },
+          periodSeconds: 30,
+          failureThreshold: 3,
+        },
+        readinessProbe: {
+          httpGet: { path: "/", port: 5000 },
+          periodSeconds: 10,
+          failureThreshold: 3,
+        },
+      },
+    ],
+  },
+  webPort: 5000,
+  subDomain: "docker",
+};
+
+export default applyModifiers(
+  base,
+  withSecurityDefaults(),
+  withNasMounts({
+    main: [{ mountPath: "/var/lib/registry", subPath: "cluster/registry" }],
+  }),
+);
