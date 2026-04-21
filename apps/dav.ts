f1e2bc3f1109d588b78edb32613f24177141fbff
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2026-04-21 14:37:43 -0400

    feat: add dav config

--- apps/dav.ts
+++ apps/dav.ts
@@ -1,8 +1,8 @@
+import { applyModifiers, withNasMounts } from "../modifiers";
 import type { WorkloadApp } from "../types";
-import {
-  applyModifiers,
-  withNasMounts,
-} from "../modifiers";
+import { buildGeneratedSecret } from "../utils";
+
+const credentialsSecretName = "dav-credentials";
 
 const base: WorkloadApp = {
   kind: "workload",
@@ -13,11 +13,40 @@ const base: WorkloadApp = {
         name: "main",
         image: "ghcr.io/aaronkyriesenbach/dav:0.0.1-SNAPSHOT",
         ports: [{ containerPort: 8080 }],
+        readinessProbe: {
+          httpGet: { path: "/healthz", port: 8080 },
+          periodSeconds: 30,
+          timeoutSeconds: 5,
+        },
+        livenessProbe: {
+          httpGet: { path: "/healthz", port: 8080 },
+          initialDelaySeconds: 5,
+          periodSeconds: 30,
+          timeoutSeconds: 5,
+        },
+        env: [
+          {
+            name: "DAV_USERS_AARON_DISPLAY_NAME",
+            value: "aaron",
+          },
+          {
+            name: "DAV_USERS_AARON_PASSWORD",
+            valueFrom: {
+              secretKeyRef: {
+                name: credentialsSecretName,
+                key: "aaron-password",
+              },
+            },
+          },
+        ],
       },
     ],
   },
   webPort: 8080,
   externallyAccessible: true,
+  extraResources: buildGeneratedSecret(credentialsSecretName, [
+    "aaron-password",
+  ]),
 };
 
 export default applyModifiers(
