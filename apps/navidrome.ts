ea7eb7d01ff4624a8ea4e644b2bdbd047e385e11
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2026-08-06 09:00:00 -0400

    feat: disable navidrome ext auth

--- apps/navidrome.ts
+++ apps/navidrome.ts
@@ -1,9 +1,4 @@
-import {
-  applyModifiers,
-  withIscsiVolumes,
-  withNasMounts,
-  withOidcAuth,
-} from "../modifiers";
+import { applyModifiers, withIscsiVolumes, withNasMounts } from "../modifiers";
 import type { WorkloadApp } from "../types";
 
 const base: WorkloadApp = {
@@ -19,19 +14,8 @@ const base: WorkloadApp = {
         ports: [{ name: "http", containerPort: 4533 }],
         env: [
           { name: "ND_MUSICFOLDER", value: "/music" },
-          { name: "ND_ADDRESS", value: "0.0.0.0" },
-          { name: "ND_PORT", value: "4533" },
           { name: "ND_SCANONSCHEDULE", value: "0 0 */6 * *" },
           { name: "ND_ENFORCENONROOTUSER", value: "true" },
-          {
-            name: "ND_EXTAUTH_TRUSTEDSOURCES",
-            value: "10.42.0.0/24,10.43.0.0/16",
-          },
-          { name: "ND_EXTAUTH_USERHEADER", value: "Remote-User" },
-          { name: "ND_EXTAUTH_LOGOUTURL", value: "https://auth.lab53.net" },
-          { name: "ND_SESSIONTIMEOUT", value: "168h" },
-          { name: "ND_ENABLESHARING", value: "true" },
-          { name: "ND_ENABLEDOWNLOADS", value: "true" },
         ],
         livenessProbe: {
           httpGet: { path: "/ping", port: 4533 },
@@ -57,14 +41,5 @@ export default applyModifiers(
   }),
   withNasMounts({
     main: [{ mountPath: "/music", subPath: "music" }],
-  }),
-  withOidcAuth({
-    middleware: {
-      enabled: true,
-      headers: [
-        { name: "Remote-User", value: "{{ .claims.preferred_username }}" },
-        { name: "Remote-Email", value: "{{ .claims.email }}" },
-      ],
-    },
   }),
 );
