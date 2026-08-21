b524314a746aa6926ac025c8a4c2ac20e762d330
Aaron Ky-Riesenbach <12665860+aaronkyriesenbach@users.noreply.github.com>
2026-08-21 09:27:38 -0400

    feat: add calino

---
+++ apps/calino.ts
@@ -0,0 +1,34 @@
+import { applyModifiers, withOidcAuth } from "../modifiers";
+import type { WorkloadApp } from "../types";
+
+const base: WorkloadApp = {
+  kind: "workload",
+  name: "calino",
+  podSpec: {
+    containers: [
+      {
+        name: "main",
+        image: "ghcr.io/ivan-malinovski/calino:0.29.1",
+        ports: [{ name: "http", containerPort: 8080 }],
+        livenessProbe: {
+          httpGet: { path: "/", port: 8080 },
+          initialDelaySeconds: 5,
+          periodSeconds: 30,
+          failureThreshold: 3,
+        },
+        readinessProbe: {
+          httpGet: { path: "/", port: 8080 },
+          periodSeconds: 10,
+          failureThreshold: 3,
+        },
+      },
+    ],
+  },
+  webPort: 8080,
+  subDomain: "calendar",
+};
+
+export default applyModifiers(
+  base,
+  withOidcAuth({ middleware: { enabled: true } }),
+);
